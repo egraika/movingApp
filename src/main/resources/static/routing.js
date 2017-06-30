3a13c22d9b65573183bbf2bfb32dcbe7929146c0
Eric <ericgraika@gmail.com>
2017-06-29 20:25:58 -0500

    ui fixes

--- src/main/resources/static/routing.js
+++ src/main/resources/static/routing.js
@@ -139,7 +139,7 @@
 		  // route change start code ...
 		 $rootScope.$on('$routeChangeStart', function(event, next) {
 			 $rootScope.path = $location.path();
-			 $rootScope.currentPath = $location.path() === '/bookmove' || $location.path() === '/contact';
+			 $rootScope.currentPath = $location.path() === '/bookmove' || $location.path() === '/contact' || $rootScope.authenticated;
 		 if (next.originalPath === "/login" && $rootScope.authenticated) {
 		   event.preventDefault();
 		  } else if (next.access && next.access.loginRequired && !$rootScope.authenticated) {
