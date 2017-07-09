c587a84a2b541b20479450880129ac20173e29a8
Eric <ericgraika@gmail.com>
2017-07-09 00:04:42 -0500

    phone number input change

--- src/main/resources/static/js/freelancer.js
+++ src/main/resources/static/js/freelancer.js
@@ -12,23 +12,23 @@
         event.preventDefault();
     });
 
-    // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
-    });
+//    // Highlight the top nav as scrolling occurs
+//    $('body').scrollspy({
+//        target: '.navbar-fixed-top',
+//        offset: 51
+//    });
 
     // Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse ul li a').click(function(){ 
             $('.navbar-toggle:visible').click();
     });
 
-    // Offset for Main Navigation
-    $('#mainNav').affix({
-        offset: {
-            top: 100
-        }
-    })
+//    // Offset for Main Navigation
+//    $('#mainNav').affix({
+//        offset: {
+//            top: 100
+//        }
+//    })
 
     // Floating label headings for the contact form
     $(function() {
