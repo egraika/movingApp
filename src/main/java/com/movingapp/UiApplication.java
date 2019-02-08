e03be751cffffe7b75da82574e63cbb9f40cfc01
Eric <ericgraika@gmail.com>
2019-02-07 19:23:09 -0600

    stripe api key by environment

--- src/main/java/com/movingapp/UiApplication.java
+++ src/main/java/com/movingapp/UiApplication.java
@@ -18,25 +18,6 @@ import java.security.Principal;
 @EnableJpaRepositories("com.movingapp.dao")
 @Import({ SecurityConfig.class })
 public class UiApplication {
-
-	//this adds http too
-//	@Bean
-//	public Integer port() {
-//		return SocketUtils.findAvailableTcpPort();
-//	}
-//
-//	@Bean
-//	public EmbeddedServletContainerFactory servletContainer() {
-//		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
-//		tomcat.addAdditionalTomcatConnectors(createStandardConnector());
-//		return tomcat;
-//	}
-//
-//	private Connector createStandardConnector() {
-//		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-//		connector.setPort(port());
-//		return connector;
-//	}
     
 	public static void main(String[] args) {
 		SpringApplication.run(UiApplication.class, args);
