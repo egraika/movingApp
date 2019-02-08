9814359ec23c5cdf91822f3ccb452f0e13ad8e80
Eric <ericgraika@gmail.com>
2019-02-07 22:14:59 -0600

    update stripe api keys

--- src/main/java/com/movingapp/Constants/MovingAppConstants.java
+++ src/main/java/com/movingapp/Constants/MovingAppConstants.java
@@ -4,7 +4,5 @@ public class MovingAppConstants {
 
     public static String apiKey = "stripe_api_key";
     public static String environment = "spring.profiles.active";
-    //public static String apiKey = "sk_test_356VkXvxAv3KPrTnpY6iJkTb";
-    //public static String apiKey = "sk_live_xi03Kbf02Hqd57v7Zc6lFyge";
 
 }
